3709a09e859ef04ede166742678db079c29fcbbb
Douglas Silva <devdouglassilva@gmail.com>
2023-11-16 14:33:56 +0000

    Change Nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,27 @@ import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   vite: {
     plugins: [
       sassGlobImports()
     ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/_colors.scss" as *;',
+        },
+      },
+      modules: {
+        generateScopedName: "[sha256:hash:base64:16]",
+      },
+    }
   },
-  css: ["~/assets/scss/main.scss"]
+  css: ["~/assets/scss/main.scss"],
 })
