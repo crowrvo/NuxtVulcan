08a4f5c26d11aa5ec86926f0e70d3435b1e532da
Douglas Silva <devdouglassilva@gmail.com>
2023-11-17 02:25:03 +0000

    Finished NovelCard

---
+++ components/Dashboard/NovelCard.vue
@@ -0,0 +1,200 @@
+<script lang="ts" setup>
+type novelInfo = {
+    name: string;
+    workAs: string;
+    views: number;
+    chapters: number;
+    comments: number;
+    url: string;
+    backgroundUrl: string;
+}
+
+defineProps<{
+    novel: novelInfo;
+}>();
+</script>
+<template>
+    <div :class="card.cardNovel">
+        <ButtonLink :to="novel.url" :class="card.cardNovelContainImage">
+            <img :src="novel.backgroundUrl" :class="card.cardNovelImage">
+        </ButtonLink>
+        <div :class="card.cardNovelInfo">
+            <h3 :class="card.cardNovelName">{{ novel.name }}</h3>
+            <span :class="card.cardNovelWork">{{ novel.workAs }}</span>
+            <div :class="card.cardNovelData">
+                <span><strong>Views</strong>: {{ novel.views }}</span>
+                <span><strong>Capítulos</strong>: {{ novel.chapters }}</span>
+                <span><strong>Comentários</strong>: {{ novel.comments }}</span>
+            </div>
+            <div :class="card.cardNovelActions">
+                <Button :class="card.add">Adicionar capítulos</Button>
+                <Button>Ver capítulos</Button>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" module="card">
+@use "@/assets/scss/settings/colors";
+
+.card-novel {
+    display: flex;
+    gap: 8px;
+    width: 100%;
+    max-width: max-content;
+    min-height: 90px;
+    height: 100%;
+    padding: 5px 20px 5px 5px;
+    background: $c-background;
+    box-shadow: 0 2px 4px transparentize($c-grayscale-1, .75);
+    border-radius: 8px;
+
+    &__contain-image {
+        display: block;
+        min-height: 100%;
+        max-height: 100%;
+        min-width: 100px;
+    }
+
+    &__image {
+        // aspect-ratio: 9/16;
+        display: block;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        border-radius: 8px;
+    }
+
+    &__info {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        padding: .5rem 0;
+    }
+
+    &__name {
+        text-transform: capitalize;
+        word-break: keep-all;
+        font-size: medium;
+        color: $c-primary-darken;
+        margin: 0;
+
+        @media (prefers-color-scheme: dark) {
+            & {
+                color: $c-white;
+            }
+        }
+    }
+
+    &__work {
+        font-size: small;
+        color: $c-grayscale-1;
+
+        @media (prefers-color-scheme: dark) {
+            & {
+                color: $c-grayscale-2;
+            }
+        }
+    }
+
+    &__data {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+        padding: 4px 0;
+
+        & span {
+            display: block;
+            font-size: small;
+            color: $c-primary-darken;
+
+            @media (prefers-color-scheme: dark) {
+                & {
+                    color: $c-primary;
+                }
+            }
+        }
+
+        & strong {
+            color: $c-grayscale-1;
+            font-weight: normal;
+
+            @media (prefers-color-scheme: dark) {
+                & {
+                    color: $c-grayscale-2;
+                }
+            }
+        }
+    }
+
+    &__actions {
+        display: flex;
+        align-items: center;
+        padding: 8px 0;
+        gap: 4px;
+        width: max-content;
+
+        & button {
+            padding: 2px 8px;
+            background-color: $c-background;
+            color: $c-primary-darken;
+            border-radius: 20px;
+            width: 100%;
+            font-size: small;
+            outline: .5px solid $c-primary-darken;
+            height: 100%;
+            transition: all 200ms ease-out;
+
+            &:hover,
+            &:active {
+                background: $c-primary-darken;
+                color: $c-white;
+                outline: none;
+            }
+
+            &.add {
+                background-color: $c-primary-darken;
+                color: $c-white;
+
+                &:hover,
+                &:active {
+                    background: $c-white;
+                    color: $c-primary-darken;
+                    outline: .5px solid $c-primary-darken;
+                }
+            }
+
+            @media (prefers-color-scheme: dark) {
+                & {
+                    background-color: $c-white;
+                    color: $c-primary-darken;
+                }
+
+                &:hover,
+                &:active {
+                    background: $c-primary-lighten;
+                    color: $c-primary-darken;
+                    outline: none;
+                }
+
+                &.add {
+                    background-color: $c-primary;
+                    color: $c-primary-darken;
+
+                    &:hover,
+                    &:active {
+                        background: $c-primary-lighten;
+                        color: $c-primary-darken;
+                        outline: none;
+                    }
+                }
+            }
+        }
+    }
+
+    @media (prefers-color-scheme: dark) {
+        & {
+            background-color: $c-dark-mode;
+        }
+    }
+}
+</style>
