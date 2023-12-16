5bef68d85b0ffc4a28b702c0a6b62f102c54ad3f
Douglas Silva <devdouglassilva@gmail.com>
2023-12-16 05:04:09 +0000

    Change appearance of route /dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,29 +1,13 @@
 <script lang="ts" setup>
 definePageMeta({
-    layout: 'dashboard'
+    layout: 'dashboard',
+    name: "dashboard",
+    path: "/dashboard",
 })
 
 import type { announcesProps } from '~/components/Dashboard/Announcement.vue';
-import type { novelInfo } from '~/components/Dashboard/Novels.vue';
-import type { PatronerProps } from '~/components/PatronerCard.vue';
-
-const novels:novelInfo[] = [{
-    name: "One Punch Man",
-    workAs: "Tradutor",
-    views: 2301313132,
-    chapters: 300,
-    comments: 12311132,
-    url: "#opm",
-    backgroundUrl: "/images/placeholder.jpg"
-}, {
-    name: "Jujutsu Kaisen",
-    workAs: "Editor",
-    views: 12312323,
-    chapters: 312,
-    comments: 201132,
-    url: "#jujustu",
-    backgroundUrl: "/images/placeholder2.jpg"
-}]
+import { mockDonators } from '~/mock/donators';
+import { mockNovels } from '~/mock/books';
 
 const announces:announcesProps[] = [{
     url: "#hum",
@@ -41,44 +25,31 @@ const announces:announcesProps[] = [{
     hour: "17:30"
 }]
 
-const patroners: PatronerProps[] = [{
-    name: "Goku",
-    contribuiteType: "Direto",
-    contribuitionValue: 23.4,
-    contribuitionDate: new Date(),
-    contribuitionTo: "Legado de Sangue",
-    contribuitionUrl: "#",
-    url: "#"
-},{
-    name: "Satoro",
-    contribuiteType: "Direto",
-    contribuitionValue: 23.4,
-    contribuitionDate: new Date(),
-    contribuitionTo: "Legado de Sangue",
-    contribuitionUrl: "#",
-    url: "#"
-}];
 </script>
 <template>
     <div :class="$style.container">
         <DashboardWelcomeUser name="Douglas" image-src="/images/placeholder.jpg" />
         <DashboardAnnouncement :announces="announces" />
-        <DashboardNovels :novels="novels"/>
-        <DashboardPatroners :patroners="patroners" :donations-goal="300" />
+        <DashboardNovels :novels="mockNovels"/>
+        <DashboardDonators :donators="mockDonators" :donations-goal="300" />
     </div>
 </template>
 <style lang="scss" module>
 .container {
     display: grid;
-    grid-auto-columns: 1fr;
+    grid-template-columns: map-get($spacing, 'common-2') [content-start] 1fr 1fr [content-end] map-get($spacing, 'common-2');
     grid-auto-rows: minmax(140px, max-content);
     grid-auto-flow: column dense;
     gap: map-get($spacing, 'biggest');
     grid-template-areas:
-        "welcome-user welcome-user welcome-user welcome-user"
-        "announcements announcements announcements announcements"
-        "novels novels novels novels"
-        "patroners patroners patroners patroners";
-    padding: 1rem;
+        ". welcome-user welcome-user ."
+        ". announcements announcements ."
+        ". novels novels ."
+        ". donators donators .";
+    padding: 1rem 0;
+
+    & > * {
+        grid-column: content;
+    }
 }
 </style>
